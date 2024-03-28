78b486159b268fabe5e01b08570b2fe2fbb3ab4d
Nikitos1865 <na2789@columbia.edu>
2024-03-28 17:58:55 -0400

    Added a method to insert into the BST

--- lib/bst.rb
+++ lib/bst.rb
@@ -3,22 +3,54 @@ require '../recursion'
 
 
 class BST
+
+    attr_accessor :root 
+
     def initialize(array)
         @root = to_balanced_tree(merge_sort(array.uniq))
     end 
 
     def to_balanced_tree(array)
-        if array.length < 1
-            return 
-        elsif array.length == 1
-            boot = BST_Node.new(array[0])
+        if array == nil || array.length < 1 
+            return nil
+        elsif array.one?
+            root = BST_Node.new(array[0])
         else 
             mid = array.length/2.0
-            boot = BST_Node.new(array[mid])
-            boot.left = to_balanced_tree(array[..mid-1])
-            boot.right = to_balanced_tree(array[1+mid..])
-            return boot
+            root = BST_Node.new(array[mid])
+            root.left = to_balanced_tree(array[..mid-1])
+            root.right = to_balanced_tree(array[1+mid..])
+            return root
         end  
+    end 
+
+    def insert(root, key)
+        if root == nil 
+            root = BST_Node.new(key)
+        else 
+            if root.data == key
+                root 
+            elsif key < root.data  
+                root.left = insert(root.left,key)
+            else 
+                root.right = insert(root.right,key)
+            end 
+        end 
+        root 
+    end 
+
+    def find(root,key)
+        if root == nil 
+            return nil
+        elsif root.data == key
+            return root
+        elsif key < root.data
+            puts "left"
+            find(root.left, key)
+        else 
+            puts "right "
+            find(root.right, key)
+        end 
     end 
 
     def pretty_print(node = @root, prefix = '', is_left = true)
@@ -33,10 +65,19 @@ p array = merge_sort([1, 7, 4, 23, 8, 9, 4, 6, 3, 5, 7, 9, 67, 6345, 324].uniq)
 
 bst = BST.new([1, 7, 4, 23, 8, 9, 4, 6, 3, 5, 7, 9, 67, 6345, 324])
 
-p 
+c = [5]
 
-bst
+bst1 = BST.new([])
+
+p bst.find(bst.root, 6345)
+
+
+
+
 
 bst.pretty_print
+
+
+
 
 
